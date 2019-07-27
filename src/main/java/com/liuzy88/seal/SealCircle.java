f5cb4d58226ae82f0ede1297835b38c796b95346
liuzy <liuzy@liuzy88.com>
2019-07-28 01:07:46 +0800

    fix

---
+++ src/main/java/com/liuzy88/seal/SealCircle.java
@@ -0,0 +1,12 @@
+package com.liuzy88.seal;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SealCircle {
+    private Integer line;
+    private Integer width;
+    private Integer height;
+}
